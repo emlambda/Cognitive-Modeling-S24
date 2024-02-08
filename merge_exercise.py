4c3f8b0b53af6d18ca658f260b1bc810b9ee15f2
zjh1379 <834943383@qq.com>
2024-02-08 13:07:03 -0500

    Create merge_exercise.py
    merge exercise

---
+++ merge_exercise.py
@@ -0,0 +1,7 @@
+# Merge Conflit 1: Edit same data from different branch
+shared_data_structure = {}
+
+def update_data_structure_developer_a(data_structure):
+    data_structure['key_a'] = 'value_from_a'
+    return data_structure
+
