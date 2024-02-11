ffedca73beecaa9e6cd15b30fd3ddb7c01278094
zjh1379 <834943383@qq.com>
2024-02-11 14:39:30 -0500

    Remove file
    Remove repeated file

--- merge_exercise.py
+++
@@ -1,9 +0,0 @@
-shared_data_structure = { 
-    'key_a': 'initial_value_a',
-    'key_b': 'initial_value_b',
-    'common_key': 'common_initial_value'
-}
-
-def update_data_structure_developer_b(data_structure):
-    data_structure['key_a'] = 'value_from_b'
-    return data_structure
